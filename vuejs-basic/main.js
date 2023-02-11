744ffcd622e3b8d7ba11696eeea2a81df2c1e349
Jon Dimaculangan <jondmclngn@gmail.com>
2023-02-11 00:19:59 -0600

    bind + event directives

--- vuejs-basic/main.js
+++ vuejs-basic/main.js
@@ -1,16 +1,30 @@
 const component = {
     data() {
-        return {
-            name: 'Jon'
+        return { // READ & WRITE data
+            name: 'Jon',
+            count: 0
+            // headerClass: 'text-green'
         }
 
     }, //method
-    computed: {
+    computed: { //READ data
         greetings() {
             if (!this.name.length) {
                 return 'Who are you?'
             }
             return `Hello, ${this.name}`
+        },
+        headerClass() {
+            return !this.name.length ? 'text-red' : 'text-green'
+        }
+    },
+    methods: { //handler for v-on
+        increment() {
+            // this.count = this.count +1
+            this.count++
+        },
+        decrement() {
+            this.count--
         }
     }
 }
